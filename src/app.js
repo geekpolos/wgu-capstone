bf1d4145241a19d1a78874c71d41ea8f20be99c3
Daniel Williams <daniel.williams@talktotucker.com>
2020-05-29 12:53:48 -0400

    Building out templates, views, and partials

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
-console.log('from app.js')
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+
+const app = express()
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, '../public/')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server started')
+})
