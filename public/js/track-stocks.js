99647fa78e5d5a7389e304ad8dd6e921c200c915
Daniel Williams <daniel.williams@talktotucker.com>
2020-05-31 22:49:04 -0400

    Move mongodb to app.js and add data to table through track-stocks.js

--- public/js/track-stocks.js
+++ public/js/track-stocks.js
@@ -1,1 +1,26 @@
 console.log('Loaded from track-stocks.js')
+
+const stockTable = document.querySelector('stock-table-body')
+
+fetch('/track-stocks/get').then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log('something went wrong')
+            //messageOne.textContent = data.error
+        } else {
+            console.log(data)
+
+            var table = document.getElementById("stock-table");
+
+            var rowNode = document.createElement("tr");
+            var cellNode = document.createElement("td");
+            var textNode = document.createTextNode(data.stock.ticker);
+
+            cellNode.appendChild(textNode);
+            rowNode.appendChild(cellNode);
+            table.appendChild(rowNode);
+
+            console.log('should be done')
+        }
+    })    
+})
