154e0ba81def9caf8d42b7adaf7ed65dd810a2bd
Daniel Williams <daniel.williams@talktotucker.com>
2020-06-03 15:31:52 -0400

    Finish out /reports page to dynamically create reports

--- public/js/reports.js
+++ public/js/reports.js
@@ -4,7 +4,15 @@ let generateButton = document.getElementById("generateButton");
 let downloadButton = document.getElementById("downloadButton");
 
 generateButton.onclick = function(){
-    downloadButton.classList.remove("disabled")
-    downloadButton.classList.remove("btn-outline-secondary")
-    downloadButton.classList.add("btn-success")
+    fetch('/reports/generate').then((response) => {
+        response.json().then((data) => {
+            if(data.error) {
+                document.getElementById("alertMessage").innerHTML = '<div class="alert alert-danger" role="alert" id="alertMessage">My Stock Tracker was unable to connect to the database. Please try again later.</div>';
+            } else {
+                downloadButton.classList.remove("disabled")
+                downloadButton.classList.remove("btn-outline-secondary")
+                downloadButton.classList.add("btn-success")
+            }
+        })
+    })    
 };
